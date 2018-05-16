a6a1aae9d10047a27ed74022e224c7e12de58c63
Helen Stone <helen.stone@asos.com>
2018-05-16 11:32:43 +0100

    Add project files.

---
+++ AzureServiceBusTopics/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace AzureServiceBusTopics
+{
+    using System;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Microsoft.Azure.ServiceBus;
+
+    internal class Program
+    {
+        private const string ServiceBusConnectionString = "Endpoint=sb://helensbnstopics.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=mBSqgAKVbAiiSxieIJ1yqfaKhlO+IFg2T8u7fbyZink=";
+
+        private const string TopicName = "firsttopic";
+
+        private static ITopicClient topicClient;
+
+        public static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        public static async Task MainAsync()
+        {
+            const int NumberOfMessages = 10;
+            topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Send messages
+            await SendMessagesAsync(NumberOfMessages);
+
+            Console.ReadKey();
+
+            await topicClient.CloseAsync();
+
+        }
+
+        private static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the topic
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message : {messageBody}");
+
+                    // Send the message to the topic.
+                    await topicClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception : {exception.Message}");
+            }
+        }
+    }
+}
