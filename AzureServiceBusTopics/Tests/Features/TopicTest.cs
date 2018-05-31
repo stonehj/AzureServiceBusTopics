98437a2e7c130c86be996d138da671344a3173c0
Helen Stone <helen.stone@asos.com>
2018-05-31 12:59:17 +0100

    finished test

--- AzureServiceBusTopics/Tests/Features/TopicTest.cs
+++ AzureServiceBusTopics/Tests/Features/TopicTest.cs
@@ -1,17 +1,27 @@
 ﻿namespace AzureServiceBusTopics.Tests.Features
 {
     using System;
+    using System.Text;
     using System.Threading.Tasks;
 
     using Microsoft.Azure.ServiceBus;
-    using AzureServiceBusTopics;
+    using Microsoft.Azure.ServiceBus.InteropExtensions;
+    using Microsoft.ServiceBus;
+    using Microsoft.ServiceBus.Messaging;
 
     using NUnit.Framework;
 
+    using Shouldly;
+
     using TestStack.BDDfy;
+
+    using SubscriptionClient = Microsoft.Azure.ServiceBus.SubscriptionClient;
+    using TopicClient = Microsoft.Azure.ServiceBus.TopicClient;
 
     class TopicTest
     {
+        public const string TEST_MESSAGE_SUB = "TestMessageSub";
+
         private const string ServiceBusConnectionString =
             "Endpoint=sb://helensbnstopics.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=mBSqgAKVbAiiSxieIJ1yqfaKhlO+IFg2T8u7fbyZink=";
 
@@ -19,33 +29,54 @@
 
         private const string TopicName = "firsttopic";
 
-        private static ITopicClient testTopicClient;
+        private static TopicClient testTopicClient;
 
-        private static ISubscriptionClient testSubscriptionClient;
+        private static SubscriptionClient testSubscriptionClient;
+
+        private static OnMessageOptions options;
+
+        private static NamespaceManager namespaceManager;
+
+        private static Message message;
 
         [Test]
         public void TopicHappyPathTest()
         {
             this.Given(_ => this.ATestTopicAndATestSubscription())
+                .And(_ => this.AMessage())
                 .When(_ => this.TheMessageIsSent())
                 .Then(_ => this.TheMessageIsPublished())
                 .BDDfy();
+        }
+
+        private void AMessage()
+        {
+            string messageBody = "Test Message";
+            message = new Message(Encoding.UTF8.GetBytes(messageBody));
         }
 
         private void ATestTopicAndATestSubscription()
         {
             testTopicClient = new TopicClient(ServiceBusConnectionString, TopicName);
             testSubscriptionClient = new SubscriptionClient(ServiceBusConnectionString, TopicName, SubscriptionName);
+
+            // create test subscription
+            namespaceManager = NamespaceManager.CreateFromConnectionString(ServiceBusConnectionString);
+            namespaceManager.CreateSubscription(TopicName, TEST_MESSAGE_SUB);
+
+            options = new OnMessageOptions() { AutoComplete = false, AutoRenewTimeout = TimeSpan.FromMinutes(1), };
         }
 
         private void TheMessageIsPublished()
         {
-            throw new NotImplementedException();
+            string receivedMessage = message.GetBody<string>();
+
+            receivedMessage.ShouldBe("Test Message");
         }
 
-        private async Task TheMessageIsSent()
+        private void TheMessageIsSent()
         {
-            await MessageSender.SendMessagesAsync(1);
+            testTopicClient.SendAsync(message);
         }
     }
 }
